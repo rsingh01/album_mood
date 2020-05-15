d3be549be85fba50e83f333000dbc1d161d99728
Raj Singh <nova.magus@gmail.com>
2020-05-15 10:17:52 -0600

    refactored into object oriented fashion -- will be useful for granular analysis later

--- vocab.py
+++ vocab.py
@@ -9,6 +9,29 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 nltk.download('punkt')
 
+class Artist: 
+
+    def __init__(self, artist_name: str, albums: list):
+        self.artist_name = artist_name
+        self.albums = albums
+
+    def print_attrs(self):
+        print(self.artist_name)
+        for album in self.albums:
+            print(album)
+
+class Album:
+    def __init__(self, album_title: str, song_list: list):
+        self.album_title = album_title
+        self.song_list = song_list
+
+class Song: 
+    def __init__(self, song_title: str, lyrics: list):
+        self.song_title = song_title
+        self.lyrics = lyrics
+
+
+
 def get_album_html() -> bs4.BeautifulSoup:
     http = httplib2.Http()
     artist_name = input("Please enter an artist name: ").capitalize()
@@ -55,8 +78,7 @@ def main():
     #unique_words = {}
     album_urls = []
     album_titles = []
-    album_sentiments = {}
-    album_dict = {}
+
     for name in album_names:
         #get album urls and titles
         album_urls.append(name['href'])
@@ -65,6 +87,7 @@ def main():
 
     print(artist_name)
     print('Albums being analyzed: ' + ', '.join(album_titles))
+    artist = Artist(artist_name, [])
 
     sentences = ""
     song_urls = []
@@ -81,15 +104,18 @@ def main():
             #get song urls
             song_urls.append(name['href'])
 
-        print("Songs in " + album_titles[x])
+        print("\nSongs in " + album_titles[x])
+        album = Album(album_titles[x], [])
+        
         #for each song
         for url in song_urls:
-            print(url)
             #go to song lyrics web paage
             new_page = requests.get(url)
             new_html = bs4.BeautifulSoup(new_page.text, "html.parser")
             #get text version of song lyrics
             lyrics = new_html.find("div", class_ = "lyrics").get_text()
+            song_name = new_html.find('h1', class_ = "header_with_cover_art-primary_info-title").get_text()
+            print(song_name)
             split_lyrics = lyrics.split('\n')
 
             #remove extraneous lyrics like [verse] or [chorus]
@@ -103,14 +129,19 @@ def main():
                     sentences = sentences.replace('\u2005', '')
                     
             sentences = sentences.split('\n')
-            album_dict[album_titles[x]] = sentences
+            song = Song(song_name, sentences)
+            album.song_list.append(song)
             sentences = ""
 
+        artist.albums.append(album)
         x = x + 1
         song_urls = []
-    
-    album_sentiments = get_sentiments(album_dict)
-    print_album_sentiments(album_sentiments)
 
+    #album_sentiments = get_sentiments(album_dict)
+    #print_album_sentiments(album_sentiments)
+    pprint.pprint(artist.__dict__)
+    for album in artist.albums:
+        pprint.pprint(album.__dict__)
+        
 if __name__ == "__main__":
     main()
